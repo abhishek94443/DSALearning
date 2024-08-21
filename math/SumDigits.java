7ff69c910cfbe6e4efe865e013f2d2d261622e1f
abhishek94443 <abhishekdwivedi9444@gmail.com>
2024-08-21 14:04:15 +0530

    sum of digits

--- math/SumDigits.java
+++ math/SumDigits.java
@@ -3,9 +3,18 @@ package math;
 import java.util.Scanner;
 
 public class SumDigits {
+    public static int digitSum(String num){
+        char []c=num.toCharArray();
+        int sum=0;
+        for (int i = 0; i < c.length; i++) {
+            sum+= (c[i]-'0');
+        }return sum;
+    }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
+
+        System.out.println(digitSum(Integer.toString(n)));
         int sum=0;
         while(n!=0){
             int num=n%10;
@@ -13,5 +22,6 @@ public class SumDigits {
             n=n/10;
         }
         System.out.println(sum);
+
     }
 }
